1e9c11aaf6c42bafa8849872b290480280a253c6
jy1655 <daunfun@gmail.com>
2023-10-26 13:16:40 +0900

    make closer

--- BLEStudy/ViewController.swift
+++ BLEStudy/ViewController.swift
@@ -16,22 +16,18 @@ class ViewController: UIViewController {
     var nameLabel: UILabel!
     var identifierLabel: UILabel!
 
-    var data: CBPeripheral? {
-        didSet {
-            print("데이터 \(data as Any) 전송 받음")
-            updatePeripheralData()
-        }
-    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(bluetoothManager.connectedPeripheral ?? "몰루?")
         setupLabels()
 
         view.backgroundColor = .white // UIViewController들이 배경색이 검정이라 흰색으로 변경
 
         touchUpScanButton() // 블루투스 스캔 버튼
         touchUpClassicButton() // 블루투스 스캔 종료
+
+        touch() // 수동 데이터 업데이트용 임시버튼
 
         print("hello 블루투스 World")
 
@@ -41,8 +37,12 @@ class ViewController: UIViewController {
     @objc func BLEButton(_ sender: UIButton) {
         let showBluetooth = ShowBluetoothModal()
         print("click BLE")
-        self.present(showBluetooth, animated: true, completion: nil)
+        self.present(showBluetooth, animated: true, completion: nil) // 모달화면 불러오기
 //        let _: () = bluetoothManager.startScanning()
+
+        showBluetooth.dismissCompletion = { [weak self] in
+            self?.updatePeripheralData()
+        } // ShowBluetoothModal의 dismissCompletion 클로저를 받아서 updatePeripheralData() 실행
     }
 
     @objc func classic(_ sender: UIButton) {
@@ -50,6 +50,11 @@ class ViewController: UIViewController {
         print("click Classic")
 //        let _: () = bluetoothManager.stopScanning()
         self.present(showClassic, animated: true, completion: nil)
+    }
+
+    @objc func updateLabel(_ sender: UIButton) {
+        let _: () = updatePeripheralData() // 클로저 오류일때 수동으로 데이터 업데이트용
+        print("수동 데이터 업데이트")
     }
 
     func touchUpScanButton() {
@@ -78,28 +83,46 @@ class ViewController: UIViewController {
         view.addSubview(button) // 화면에 버튼 구현
     }
 
+
+    func touch() {
+        let button = UIButton(type: .system) // .system = 기본적인 버튼 스타일
+
+        button.setTitle("Cc", for: .normal)
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.frame = CGRect(x: 100, y: 300, width: 50, height: 50) // 버튼 속성
+
+        button.addTarget(self, action: #selector (updateLabel), for: .touchUpInside) // 버튼에 연결될 기능
+
+        view.addSubview(button)
+    }
+
     private func setupLabels() {
         // 레이블 초기화 및 레이아웃 설정
         nameLabel = UILabel(frame: CGRect(x: 40, y: 400, width: 300, height: 30))
         nameLabel.backgroundColor = .red
         nameLabel.textColor = .black
         nameLabel.font = .boldSystemFont(ofSize: 5)
+        nameLabel.text = ""
         identifierLabel = UILabel(frame: CGRect(x: 40, y: 440, width: 300, height: 30))
         identifierLabel.backgroundColor = .green
         identifierLabel.textColor = .black
+        identifierLabel.text = ""
 
         view.addSubview(nameLabel)
         view.addSubview(identifierLabel)
+
+        print("레이블 세팅")
     }
 
-
-
     private func updatePeripheralData() {
-
-        nameLabel?.text = "Name: \(bluetoothManager.connectedPeripheral?.name ?? "Unknown")" // 옵셔널 체인 사용
-        identifierLabel?.text = "Identifier: \(bluetoothManager.connectedPeripheral?.identifier.uuidString ?? "nil???")"  // 옵셔널 체인 사용
+        
         print(bluetoothManager.connectedPeripheral?.name ?? "이름 못받음")
-        print(data?.identifier.uuidString ?? "식별번호 못받음")
+        print(bluetoothManager.connectedPeripheral?.identifier.uuidString ?? "식별번호 못받음")
+        DispatchQueue.main.async { // 메인 스레드에서 비동기로 Label을 데이터에 맞게 업데이트
+            self.nameLabel?.text = "Name: \(self.bluetoothManager.connectedPeripheral?.name ?? "Unknown name")" // 옵셔널 체인 사용
+            self.identifierLabel?.text = "Identifier: \(self.bluetoothManager.connectedPeripheral?.identifier.uuidString ?? "Unknown identifier number")"  // 옵셔널 체인 사용
+        }
     }
 
 }
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # iOSBluetooth
 
 
-- Study on How to Use Bluetooth in IOS
+## Studying How to Use Bluetooth in iOS
  
-    - 2023/10/23 corebluetooth 프레임워크를 이용한 bluetooth 사용 프로젝트 시작\r
-    - 2023/10/25 corebluetooth 프레임워크만으로는 너무 제한적이라 ExternalAccessory 프레임워크도 병행해서 사용하려 했으나 ExternalAccessory 프레임워크도 MFi인증이 되어있는 기기를 사용해야 해서 제한적인것은 마찬가지\r
+- 2023/10/23    corebluetooth 프레임워크를 이용한 bluetooth 사용 프로젝트 시작\r
+- 2023/10/25    corebluetooth 프레임워크만으로는 너무 제한적이라 ExternalAccessory 프레임워크도 병행해서 사용하려 했으나 ExternalAccessory 프레임워크도 MFi인증이 되어있는 기기를 사용해야 해서 제한적인것은 마찬가지\r
                 파일 이름 변경했다가 꼬여서 pull 해서 다시 받음 뭔가가 이상해진거 같은데 해결법을 모르겠음... 파일경로가 바뀌면 메소드가 상속을 받지 못한다\r
                 BLE로 블루투스 연결을 하긴 했는데 실제로 구현할 기능을 위해서는 실제 블루투스 모듈을 받아와야 가능할거 같다.\r
-    - 2023/10/
+- 2023/10/26    BLE통신으로 받아온 데이터를 모달창이 닫히면서 Label에 정보를 나타내는 기능 구현이 didSet, viewWillApear에서 문제가 생겨서 클로저를 구현해서 정상적으로 작동되는것을 확인\r
+                
  
-- working on it
-- This Project Was Tested on iPhone 12 Pro
+#### - This Project was Tested on iPhone 12 Pro
