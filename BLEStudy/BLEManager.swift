b502a6ecbe2f8f1bbea890c99c30059aae588a85
jy1655 <daunfun@gmail.com>
2023-10-27 14:42:56 +0900

    23/10/27 Updates early

--- BLEStudy/BLEManager.swift
+++ BLEStudy/BLEManager.swift
@@ -20,6 +20,7 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
     var restoreIdentifier: String?
     var discoveredPeripherals: [CBPeripheral] = []
     var completion: ((Bool) -> Void)?
+    var writableCharacteristic: CBCharacteristic?
     weak var tableView: UITableView?
     
 
@@ -30,16 +31,19 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
         self.centralManager = CBCentralManager(delegate: self, queue: nil, options: options as [String : Any])
             }
 
+
     private func setupCentralManager() {
             centralManager = CBCentralManager(delegate: self, queue: nil) // CBCentralManager 객체를 초기화
         // delegate: self: CBCentralManager의 델리게이트(delegate)를 현재 클래스(즉, self)로 설정합니다. 이렇게 하면 CBCentralManager에서 발생하는 이벤트(예: Bluetooth 상태 변경, 주변 장치 발견 등)에 대한 콜백을 현재 클래스에서 받을 수 있습니다. 이를 위해서는 현재 클래스가 CBCentralManagerDelegate 프로토콜을 준수해야 합니다.
         // queue: nil: 이 매개변수는 델리게이트 메서드가 실행될 디스패치 큐(dispatch queue)를 지정합니다. nil로 설정하면 메인 큐에서 델리게이트 메서드가 실행됩니다. 특정 큐에서 이벤트를 처리하려면 해당 큐를 지정할 수 있습니다.
         }
 
+
     func connect(to peripheral: CBPeripheral) {
         connectedPeripheral = peripheral
         centralManager.connect(peripheral, options: nil)
     }
+
 
         func startScanning() {
             if centralManager.state == .poweredOn { // Bluetooth 중앙 관리자의 상태를 나타내는 state라는 속성이 .poweredOn이면
@@ -56,6 +60,7 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
             centralManager.stopScan()
             print("스캔 중지...")
         } // 주변 장치 스캔 중지
+
 
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         switch central.state {
@@ -75,6 +80,7 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
                 print("Unknown state")
             }
     } // 델리게이트 메서드 centralManagerDidUpdateState(_:)는 Bluetooth 상태의 변경을 처리하기 위해 구현
+
 
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
                         advertisementData: [String: Any], rssi RSSI: NSNumber) {
@@ -99,10 +105,12 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
         }
     }
 
+
     func startConnection(completion: @escaping (Bool) -> Void) {
             self.completion = completion
             // callback 함수를 실행시키기 위한 연결 메소드
         }
+
 
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("Connected to \(peripheral.name ?? "a device")")
@@ -117,11 +125,13 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
         peripheral.discoverServices(nil)
     }
 
+
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
         print("Failed to connect to \(peripheral.name ?? "a device"). Error: \(error?.localizedDescription ?? "Unknown error")")
         completion?(false) //연결 실패 콜백 호출
         completion = nil  // 클로저를 호출한 후에는 nil로 설정하여 메모리 누수를 방지
     } // 연결 시도 후 실패했을때 실행되는 오류 메세지
+
 
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         if let error = error {
@@ -131,6 +141,7 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
         } // 연결 끊김 후 실행되는 오류 메세지
     }
 
+
     func centralManager(_ central: CBCentralManager, willRestoreState dict: [String : Any]) {
         if let restoredPeripherals = dict[CBCentralManagerRestoredStatePeripheralsKey] as? [CBPeripheral] { // 복원된 주변 장치들을 처리합니다.
             for peripheral in restoredPeripherals { // 예를 들어, 복원된 주변 장치를 discoveredPeripheral 변수에 저장합니다.
@@ -139,13 +150,14 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
         }
     }
 
+
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         if let error = error {
             print("Error discovering services: \(error.localizedDescription)")
             return
         } // 오류 확인
-        guard let services = peripheral.services else { return }
-        print("서비스 검색 오류 없음")
+        guard let services = peripheral.services else { print("서비스 검색 없음?") 
+            return }
 
             for service in services {
                 // 원하는 서비스의 UUID를 확인합니다.
@@ -162,29 +174,37 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
             }
     }
 
+
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         if let error = error {
             print("Error discovering characteristics: \(error.localizedDescription)")
             return
         }
 
-        guard let characteristics = service.characteristics else { return }
+        guard let characteristics = service.characteristics else 
+        {
+            print("서비스가 없나?")
+            return
+        }
 
             for characteristic in characteristics {
                 print("Discovered characteristic: \(characteristic)")
-
                 // 특정 특성에 대해 특정 작업을 수행하려면, 해당 특성의 UUID를 확인하고 조건부로 작업을 수행하세요.
                 // 예를 들어, 특정 특성의 값을 읽으려면 다음과 같이 할 수 있습니다.
                 // if characteristic.uuid == CBUUID(string: "특정 특성의 UUID") {
                 //     peripheral.readValue(for: characteristic)
                 // }
-
+                self.writableCharacteristic = characteristic
                 // 또는 모든 특성의 값을 읽거나 구독하려면 다음과 같이 할 수 있습니다.
                 peripheral.readValue(for: characteristic)
-                peripheral.setNotifyValue(true, for: characteristic)
-            }
 
-    } // 서비스와 특성 탐색 (이미 발견된 상황을 가정합니다)
+                if characteristic.properties.contains(.notify) {
+                    peripheral.setNotifyValue(true, for: characteristic) // 해당 특성에 대한 알림을 활성화하여 구독합니다.
+//                    print("해당 특성에 대한 알림을 활성화 (구독): \(characteristic)") // didUpdateNotificationStateFor로 호출되므로 필요가 없음
+                }
+            }
+    } // 서비스와 특성 탐색(이미 발견된 상황을 가정)후 구독하기
+
 
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         if let error = error {
@@ -193,11 +213,54 @@ class BluetoothLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDeleg
         }
 
         if let data = characteristic.value {
-            // 여기서 data를 사용하여 필요한 작업을 수행합니다.
+            // 여기서 data(특성값)를 사용하여 필요한 작업을 수행합니다.
             // 예를 들면, data를 문자열로 변환하거나 다른 형식으로 처리할 수 있습니다.
             let valueString = String(data: data, encoding: .utf8)
             print("Read value: \(valueString ?? "unknown")")
         }
-    } // peripheral(_:didUpdateValueFor:error:) 메서드에서 읽은 값을 처리합니다.
+    } // 특성값이 변경될 때마다 알림을 받습니다. peripheral(_:didUpdateValueFor:error:) 메서드에서 읽은 값을 처리합니다.
+
+
+    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error changing notification state: \(error.localizedDescription)")
+        } else {
+            if characteristic.isNotifying {
+                print("Notification began on \(characteristic)")
+            } else {
+                print("Notification stopped on \(characteristic). Disconnecting")
+                centralManager.cancelPeripheralConnection(peripheral) // 이건 주석으로 남겨두는게 좋을지 모르겠음 있을시 확실히 연결을 끊음
+            }
+        }
+    } // didDiscoverCharacteristicsFor에서 구독(setNotifyValue)한 알림상태가 변경될떄 호출됨
+
+
+
+
+
+
+
+
+
+
+
+
+    func writeValue(toCharacteristic characteristic: CBCharacteristic, onPeripheral peripheral: CBPeripheral, value: Data) {
+        if characteristic.properties.contains(.write) { // 만약 특성이 쓰기를 지원한다면
+            peripheral.writeValue(value, for: characteristic, type: .withResponse)
+        } else if characteristic.properties.contains(.writeWithoutResponse) { // 쓰기를 지원하지 않지만 만약 특성이 응답 없이 쓰기를 지원한다면
+            peripheral.writeValue(value, for: characteristic, type: .withoutResponse)
+        }
+    } // 원하는 특성을 찾아서 해당 특성에 값을 쓸 수 있도록 하는것
+
+
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error writing value: \(error.localizedDescription)")
+        } else {
+            print("Value written to \(characteristic)")
+        }
+    } // BLE 장치의 특정 특성에 값을 쓸 때 호출됩니다. 중요한 것은 쓰기 가능한 특성을 찾고, 올바른 형식의 데이터를 적절한 쓰기 타입(withResponse 또는 withoutResponse)으로 전송하는 것입니다.
+
 
 }
--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 
 ## Studying How to Use Bluetooth in iOS
  
-- 2023/10/23    corebluetooth 프레임워크를 이용한 bluetooth 사용 프로젝트 시작\r
-- 2023/10/25    corebluetooth 프레임워크만으로는 너무 제한적이라 ExternalAccessory 프레임워크도 병행해서 사용하려 했으나 ExternalAccessory 프레임워크도 MFi인증이 되어있는 기기를 사용해야 해서 제한적인것은 마찬가지\r
-                파일 이름 변경했다가 꼬여서 pull 해서 다시 받음 뭔가가 이상해진거 같은데 해결법을 모르겠음... 파일경로가 바뀌면 메소드가 상속을 받지 못한다\r
-                BLE로 블루투스 연결을 하긴 했는데 실제로 구현할 기능을 위해서는 실제 블루투스 모듈을 받아와야 가능할거 같다.\r
-- 2023/10/26    BLE통신으로 받아온 데이터를 모달창이 닫히면서 Label에 정보를 나타내는 기능 구현이 didSet, viewWillApear에서 문제가 생겨서 클로저를 구현해서 정상적으로 작동되는것을 확인\r
-                peripheral 함수 didDiscoverServices callback이 안불러지는 현상 발견해서 해결함 if문을 잘못 설정해서 탈출해버린게 문제였던듯?\r
-                
+- 2023/10/23    corebluetooth 프레임워크를 이용한 bluetooth 사용 프로젝트 시작<br/>
+- 2023/10/25    corebluetooth 프레임워크만으로는 너무 제한적이라 ExternalAccessory 프레임워크도 병행해서 사용하려 했으나 ExternalAccessory 프레임워크도 MFi인증이 되어있는 기기를 사용해야 해서 제한적인것은 마찬가지<br/>
+                파일 이름 변경했다가 꼬여서 pull 해서 다시 받음 뭔가가 이상해진거 같은데 해결법을 모르겠음... 파일경로가 바뀌면 메소드가 상속을 받지 못한다<br/>
+                BLE로 블루투스 연결을 하긴 했는데 실제로 구현할 기능을 위해서는 실제 블루투스 모듈을 받아와야 가능할거 같다.<br/>
+- 2023/10/26    BLE통신으로 받아온 데이터를 모달창이 닫히면서 Label에 정보를 나타내는 기능 구현이 didSet, viewWillApear에서 문제가 생겨서 클로저를 구현해서 정상적으로 작동되는것을 확인<br/>
+                peripheral 함수 didDiscoverServices callback이 안불러지는 현상 발견해서 해결함 if문을 잘못 설정해서 탈출해버린게 문제였던듯?<br/>
+- 2023/10/27    통신으로 통해 특성값에 정보를 보내기 직전까지 작업을 하였으나 막상 수신할만한 기기의 부재로 일단 작업 일시정지. 현재 jetson을 통한 BLE 통신관련 디벨롭을 진행하면서 다시 코드 작성예정<br/>
  
 #### - This Project was Tested on iPhone 12 Pro
